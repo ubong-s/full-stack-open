78725313bf2abeaf7d3ba788e020d8ee861f2dbc
Ubong Sylvester <ubongsly@gmail.com>
2022-09-16 16:00:16 +0100

    part1 courseinfo completed fixed object

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -17,15 +17,15 @@ const Content = (props) => {
    return (
       <div>
          <Part
-            title={props.parts[0].title}
+            name={props.parts[0].name}
             exercises={props.parts[0].exercises}
          />
          <Part
-            title={props.parts[1].title}
+            name={props.parts[1].name}
             exercises={props.parts[1].exercises}
          />
          <Part
-            title={props.parts[2].title}
+            name={props.parts[2].name}
             exercises={props.parts[2].exercises}
          />
       </div>
@@ -49,17 +49,14 @@ const App = () => {
       name: 'Half Stack application development',
       parts: [
          {
-            id: 1,
             name: 'Fundamentals of React',
             exercises: 10,
          },
          {
-            id: 2,
             name: 'Using props to pass data',
             exercises: 7,
          },
          {
-            id: 3,
             name: 'State of a component',
             exercises: 14,
          },
