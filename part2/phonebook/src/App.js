a715108068f5586681bdaa1fa153264f95a35682
Ubong Sylvester <ubongsly@gmail.com>
2022-09-19 15:33:45 +0100

    completed phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,23 @@
 import { useState } from 'react';
 
 const App = () => {
-   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+   const [persons, setPersons] = useState([
+      { name: 'Arto Hellas', number: '040-123456', id: 1 },
+      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+   ]);
+   const [filteredPersons, setFilteredPersons] = useState([...persons]);
    const [newName, setNewName] = useState('');
    const [newNumber, setNewNumber] = useState('');
+   const [search, setSearch] = useState('');
+
+   const generateId = () => Math.max(...persons.map((person) => person.id)) + 1;
 
    const handleSubmit = (e) => {
       e.preventDefault();
-      const newPerson = { name: newName, number: newNumber };
+
+      const newPerson = { name: newName, number: newNumber, id: generateId() };
 
       const doesNameExists = persons.find(
          (person) => person.name.toLowerCase() === newPerson.name.toLowerCase()
@@ -17,7 +27,9 @@ const App = () => {
          alert(`${newName} is already added to phonebook`);
       } else if (newName && newNumber) {
          setPersons(persons.concat(newPerson));
+         setFilteredPersons(persons.concat(newPerson));
          setNewName('');
+         setNewNumber('');
       }
    };
 
@@ -29,9 +41,22 @@ const App = () => {
       setNewNumber(e.target.value);
    };
 
+   const handleFilter = (e) => {
+      setSearch(e.target.value);
+
+      const tempResults = persons.filter((person) =>
+         person.name.toLowerCase().includes(search.toLowerCase())
+      );
+      setFilteredPersons(tempResults);
+   };
+
    return (
       <div>
          <h2>Phonebook</h2>
+         <div>
+            Filter shown with <input value={search} onChange={handleFilter} />
+         </div>
+         <h3>Add a new</h3>
          <form onSubmit={handleSubmit}>
             <div>
                name: <input value={newName} onChange={handleNameChange} />
@@ -44,8 +69,8 @@ const App = () => {
             </div>
          </form>
          <h2>Numbers</h2>
-         {persons.map((person, i) => (
-            <p key={i}>
+         {filteredPersons.map((person) => (
+            <p key={person.id}>
                {person.name} {person.number}
             </p>
          ))}
