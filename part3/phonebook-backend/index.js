41ffa81326158636009e3e2d89e48faf7de6abaa
Ubong Sylvester <ubongsly@gmail.com>
2022-10-04 23:57:56 +0100

    completed phonebook-backend step3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -28,14 +28,26 @@ app.get('/', (request, response) => {
    response.send(`<h1>Phonebook Backend</h1>`);
 });
 
-app.get('/api/persons', (request, response) => {
-   response.status(200).json(persons);
-});
-
 app.get('/info', (request, response) => {
    response.send(`<p>Phonebook has info for ${persons.length} people</p>
    <p>${new Date()}</p>
    `);
+});
+
+app.get('/api/persons', (request, response) => {
+   response.status(200).json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+   const { id } = request.params;
+
+   const person = persons.find((p) => p.id === Number(id));
+
+   if (!person) {
+      return response.status(404).send('Resource does not exist');
+   }
+
+   response.status(200).json(person);
 });
 
 const PORT = 3001;
