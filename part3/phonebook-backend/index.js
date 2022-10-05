8ee0a31d99bce201dbca0975ccf2b2a1d5991e6e
Ubong Sylvester <ubongsly@gmail.com>
2022-10-05 11:11:30 +0100

    completed phonebook-backend step8 configured morgan to log POST request.body

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -4,7 +4,18 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
-app.use(morgan('tiny'));
+
+morgan.token('body', function (request, response) {
+   if (request.method === 'POST') {
+      return JSON.stringify(request.body);
+   }
+
+   return ' ';
+});
+
+app.use(
+   morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
 
 let persons = [
    {
