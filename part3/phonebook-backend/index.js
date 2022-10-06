2774c2d34566448ec5fcd1a5c71a6a67f9e9d6b0
Ubong Sylvester <ubongsly@gmail.com>
2022-10-07 00:25:14 +0100

    completed phonebook-backend-database step6

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -21,33 +21,35 @@ app.use(
 app.use(cors());
 app.use(express.static('build'));
 
-let persons = [
-   {
-      id: 1,
-      name: 'Arto Hellas',
-      number: '040-123456',
-   },
-   {
-      id: 2,
-      name: 'Ada Lovelace',
-      number: '39-44-5323523',
-   },
-   {
-      id: 3,
-      name: 'Dan Abramov',
-      number: '12-43-234345',
-   },
-   {
-      id: 4,
-      name: 'Mary Poppendieck',
-      number: '39-23-6423122',
-   },
-];
+// let persons = [
+//    {
+//       id: 1,
+//       name: 'Arto Hellas',
+//       number: '040-123456',
+//    },
+//    {
+//       id: 2,
+//       name: 'Ada Lovelace',
+//       number: '39-44-5323523',
+//    },
+//    {
+//       id: 3,
+//       name: 'Dan Abramov',
+//       number: '12-43-234345',
+//    },
+//    {
+//       id: 4,
+//       name: 'Mary Poppendieck',
+//       number: '39-23-6423122',
+//    },
+// ];
 
 app.get('/info', (request, response) => {
-   response.send(`<p>Phonebook has info for ${persons.length} people</p>
-   <p>${new Date()}</p>
-   `);
+   Person.find({}).then((persons) => {
+      response.send(`<p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>
+      `);
+   });
 });
 
 app.get('/api/persons', (request, response) => {
