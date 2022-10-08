0ec66640f388798eaff016e7dddddd6978f2dcff
Ubong Sylvester <ubongsly@gmail.com>
2022-10-08 21:58:49 +0100

    bloglist step2 refactored pplication

---
+++ part4/bloglist/app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const blogRouter = require('./controllers/blog');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
+
+mongoose
+   .connect(config.MONGODB_URI)
+   .then(() => {
+      logger.info('connected to MongoDB');
+   })
+   .catch((error) => {
+      logger.error('Error connecting to MongoDB');
+   });
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use('/api/blogs', blogRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
