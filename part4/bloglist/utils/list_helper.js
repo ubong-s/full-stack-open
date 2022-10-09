8e48ba2d5a17ce13c54b178a46934f2dc00c3b1f
Ubong Sylvester <ubongsly@gmail.com>
2022-10-09 14:39:01 +0100

    bloglist helper functions and unit tests step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -11,4 +11,17 @@ const totalLikes = (blogs) => {
    return blogs.reduce(reducer, 0);
 };
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+   const sortedBlogs = blogs.sort((a, b) => (a.likes > b.likes ? -1 : 1));
+   let result = {};
+
+   if (blogs.length > 0) {
+      result.title = sortedBlogs[0].title;
+      result.author = sortedBlogs[0].author;
+      result.likes = sortedBlogs[0].likes;
+   }
+
+   return blogs.length > 0 ? result : null;
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
