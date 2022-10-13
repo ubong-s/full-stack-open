aa446d6c955eb96f0c5be0f4ba7cb164977b6269
Ubong Sylvester <ubongsly@gmail.com>
2022-10-13 20:42:37 +0100

    blog list expansion step3

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const blogRouter = require('./controllers/blog');
+const usersRouter = require('./controllers/users');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const middleware = require('./utils/middleware');
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
