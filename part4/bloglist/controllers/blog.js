90c6f055422427b161b6ec21ae30346531840740
Ubong Sylvester <ubongsly@gmail.com>
2022-10-14 12:01:27 +0100

    blog list expansion step8

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -4,24 +4,13 @@ const { query } = require('express');
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
-const getFromToken = (request) => {
-   const authorization = request.get('authorization');
-
-   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-      return authorization.substring(7);
-   }
-
-   return null;
-};
-
 blogRouter.get('/', async (request, response) => {
    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
    response.json(blogs);
 });
 
 blogRouter.post('/', async (request, response) => {
-   const body = request.body;
-   const token = getFromToken(request);
+   const { body, token } = request;
 
    const decodedToken = jwt.verify(token, process.env.SECRET);
 
