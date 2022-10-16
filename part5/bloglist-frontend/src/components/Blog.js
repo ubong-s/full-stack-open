ea1625d23f3675deadde87d6698971c8eb272eac
Ubong Sylvester <ubongsly@gmail.com>
2022-10-16 23:07:58 +0100

    bloglist frontend, step7 completed

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,35 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+   const [showDetails, setShowDetails] = useState(false);
+
+   const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5,
+   };
+
+   const toggleShowDetails = () => setShowDetails(!showDetails);
+
+   return (
+      <div style={blogStyle}>
+         <div>
+            {blog.title}
+            <button onClick={toggleShowDetails}>
+               {showDetails ? 'hide' : 'view'}
+            </button>
+         </div>
+         <div style={{ display: showDetails ? '' : 'none' }}>
+            <div>{blog.url}</div>
+            <div>
+               likes {blog.likes} <button>like</button>
+            </div>
+            <div>{blog.author}</div>
+         </div>
+      </div>
+   );
+};
+
+export default Blog;
