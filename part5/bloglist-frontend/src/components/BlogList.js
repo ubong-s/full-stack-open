a768fde481ce87601f06ddcd5aad0ecbf7e20d76
Ubong Sylvester <ubongsly@gmail.com>
2022-10-17 12:30:35 +0100

    bloglist frontend, step9  completed

--- part5/bloglist-frontend/src/components/BlogList.js
+++ part5/bloglist-frontend/src/components/BlogList.js
@@ -3,9 +3,11 @@ import Blog from './Blog';
 const BlogList = ({ blogs, updateLikes }) => {
    return (
       <>
-         {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} updateLikes={updateLikes} />
-         ))}
+         {blogs
+            .sort((a, b) => b.likes - a.likes)
+            .map((blog) => (
+               <Blog key={blog.id} blog={blog} updateLikes={updateLikes} />
+            ))}
       </>
    );
 };
