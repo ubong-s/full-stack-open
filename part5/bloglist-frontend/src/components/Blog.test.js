eee628784e22664e5fc34cb21c8e2d894f9d2542
Ubong Sylvester <ubongsly@gmail.com>
2022-10-21 22:22:52 +0100

    log list tests, step2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,8 +1,11 @@
 import '@testing-library/jest-dom/extend-expect';
 import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 
-test('<Blog/> renders only blog title and author by default', () => {
+describe('Blog', () => {
+   let container;
+
    const blog = {
       id: '5a422b3a1b54a676234d17f9',
       title: 'Canonical string reduction',
@@ -14,23 +17,45 @@ test('<Blog/> renders only blog title and author by default', () => {
    const updateLikesHandler = jest.fn();
    const deleteBlogHandler = jest.fn();
 
-   const { container } = render(
-      <Blog
-         blog={blog}
-         updateLikes={updateLikesHandler}
-         deleteBlog={deleteBlogHandler}
-      />
-   );
+   beforeEach(() => {
+      container = render(
+         <Blog
+            blog={blog}
+            updateLikes={updateLikesHandler}
+            deleteBlog={deleteBlogHandler}
+         />
+      ).container;
+   });
 
-   const author = screen.getByText(blog.author);
-   const title = screen.getByText(blog.title);
-   const url = screen.queryByText(blog.url);
-   const likes = container.querySelector('.likes');
-   const toggledSection = container.querySelector('.toggled-section');
+   test('<Blog/> renders only blog title and author by default', () => {
+      const author = screen.getByText(blog.author);
+      const title = screen.getByText(blog.title);
+      const url = screen.queryByText(blog.url);
+      const likes = container.querySelector('.likes');
+      const toggledSection = container.querySelector('.toggled-section');
 
-   expect(author).toBeDefined();
-   expect(title).toBeDefined();
-   expect(url).not.toBeInTheDocument();
-   expect(likes).not.toBeInTheDocument();
-   expect(toggledSection).not.toBeInTheDocument();
+      expect(author).toBeDefined();
+      expect(title).toBeDefined();
+      expect(url).not.toBeInTheDocument();
+      expect(likes).not.toBeInTheDocument();
+      expect(toggledSection).not.toBeInTheDocument();
+   });
+
+   test('blog url and number of likes are shown when the button controlling the shown details has been clicked', async () => {
+      const user = userEvent.setup();
+      const button = screen.getByText('view');
+      await user.click(button);
+
+      const author = screen.getByText(blog.author);
+      const title = screen.getByText(blog.title);
+      const url = screen.queryByText(blog.url);
+      const likes = container.querySelector('.likes');
+      const toggledSection = container.querySelector('.toggled-section');
+
+      expect(author).toBeDefined();
+      expect(title).toBeDefined();
+      expect(url).toBeInTheDocument();
+      expect(likes).toBeInTheDocument();
+      expect(toggledSection).toBeInTheDocument();
+   });
 });
